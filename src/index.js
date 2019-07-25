3372b827ea6939bf34748db70f3f063136f7f432
Julio Bell <juliobell2014@gmail.com>
2019-07-24 22:14:16 -0400

    Updated all files and folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+//This file is the JavaScript entry point.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
